e747f2afd338164259c349c70ca7bf726682b804
LYH2263 <lyh2263@users.noreply.github.com>
2026-08-20 11:22:48 +0800

    修复：健康探测编排-bug08

--- probe_wait.go
+++ probe_wait.go
@@ -17,7 +17,12 @@ func (sleepWaiter) Wait(ctx context.Context, d time.Duration) error {
 	if d <= 0 {
 		return nil
 	}
-	// BUG: 不听 ctx，固定 Sleep
-	time.Sleep(d)
-	return nil
+	t := time.NewTimer(d)
+	defer t.Stop()
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case <-t.C:
+		return nil
+	}
 }
